9d6ba7b61ee366e6f4ea6e1a8592a06fe78c414c
bulbik_laptop <bulbik1500@gmail.com>
2022-05-09 15:32:29 +0200

    working part of list

--- parts_of_list.php
+++ parts_of_list.php
@@ -1,37 +1,32 @@
 <?php
 
-$a = ["I", "wish", "I", "hadn't", "come"];
-function PartsOfList($a)
+$arr = ["I", "wish", "I", "hadn't", "come"];
+function PartsOfList($arr)
 {
-    $newArr = [];
-    $arryToReturn = [];
-    $aa =  $a;
+// create new arrys to oprate on
+$aa = $arr;
+// create empty arry to fill up with wanted values
+$wantedValues = [];
+$arryToReturn = [];
+$i = 0;
+foreach($arr as $item){
+    if ($i < count($arr) - 1) {
+        array_push($wantedValues, $item);
+        $secendimplode = implode(" ", $wantedValues);
+        //pushing and imploding value 
+        unset($aa[count($arr) - count($arr) + $i]);
+        $implodeValue = implode(" ", $aa);
+        //deleting and imploding from first to last index 
+        array_push($arryToReturn, [$secendimplode, $implodeValue]);
+        $i++;
+        //adding wanted values to retun array
+            } else {
+                 break;
+                }
+    }
 
-    $i = 0;
-    foreach ($a as $valu) {
-        if ($i < count($a) - 1) {
-            array_push($newArr, $valu);
-            unset($aa[count($a) - count($a) + $i]);
-            array_push($arryToReturn, $newArr, $aa);
-            $i++;
-        } else {
-            break;
-        }
-    }
-    $temp = [];
-    $y = 0;
-    foreach ($arryToReturn as $item) {
-        foreach ($item as $value) {
-            array_push($temp , $item[$y],$item[$y + 1]);
-            $y + 2;
-        }
-    }
-    return $arryToReturn;
+return $arryToReturn;
+
 }
 
-print_r(PartsOfList($a));
-// $mergre = [];
-// foreach($arryToReturn as $item){
-//     $mergre = array_merge($mergre, $item);
-// }
-// $implode = implode(" ",$mergre);
+print_r(PartsOfList($arr));
